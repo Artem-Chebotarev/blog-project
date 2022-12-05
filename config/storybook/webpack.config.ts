621971736a0033a965f9d4037c95e11e07f26bc9
Artem Chebotarev <artemchebotarev@Artems-MacBook-Pro.local>
2022-12-05 12:43:56 +0300

    some fixes

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -2,7 +2,6 @@ import webpack, { RuleSetRule } from 'webpack';
 import path from 'path';
 import { BuildPaths } from '../build/types/config';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
-import { rules } from '../../.eslintrc';
 
 export default ({ config }: {config: webpack.Configuration}) => {
     const paths: BuildPaths = {
