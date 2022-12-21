c7d736cbbd06e096598bd50df809e3db442834f7
Artem Chebotarev <artemchebotarev@Artems-MacBook-Pro.local>
2022-12-21 16:57:12 +0300

    test ci/cd

---
+++ src/shared/ui/Applink/Applink.tsx
@@ -0,0 +1,36 @@
+import { classNames } from 'shared/lib/helpers/classNames/classNames';
+import { Link, LinkProps } from 'react-router-dom';
+import { memo, ReactNode } from 'react';
+import cls from './Applink.module.scss';
+
+export enum ApplinkTheme {
+    PRIMARY = 'primary',
+    SECONDARY = 'secondary',
+    RED = 'red',
+}
+
+interface ApplinkProps extends LinkProps {
+    className?: string;
+    theme?: ApplinkTheme;
+    children?: ReactNode;
+}
+
+export const Applink = memo((props: ApplinkProps) => {
+    const {
+        to,
+        className,
+        children,
+        theme = ApplinkTheme.PRIMARY,
+        ...otherProps
+    } = props;
+
+    return (
+        <Link
+            to={to}
+            className={classNames(cls.AppLink, { [cls[theme]]: true }, [className])}
+            {...otherProps}
+        >
+            {children}
+        </Link>
+    );
+});
