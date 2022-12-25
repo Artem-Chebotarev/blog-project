fa697b4c9afc9ce71c34a4158c648ede7f12a09b
Artem Chebotarev <artemchebotarev@Artems-MacBook-Pro.local>
2022-12-25 12:46:29 +0300

    api config is fixed

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,9 +4,15 @@ import { USER_LOCAL_STORAGE_KEY } from 'shared/const/localStorage';
 // создаем инстанс axios, чтобы удобнее было работать
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
+});
+
+// interceptors - перехватчик, которй отрабатывает перед каким-то действием
+// т. е. перед тем как отправлять любой запрос у нас будет отрабатывать этот interceptor
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
         // имитация авторизации, так как на бэке проверяется только наличие этого
         // заголовка, а не сам токен
-        authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '',
-    },
+        config.headers.authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '';
+    }
+    return config;
 });
