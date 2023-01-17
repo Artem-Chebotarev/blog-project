6622e01c1e9d2c3c76cbd55b5cd00979ba32f02f
Artem Chebotarev <artemchebotarev@Artems-MacBook-Pro.local>
2023-01-17 23:37:08 +0300

    tests for articleDetailsCommentsSlice fulfilled are ready

--- src/pages/ArticleDetailsPage/model/slice/articleDetailsCommentsSlice.test.ts
+++ src/pages/ArticleDetailsPage/model/slice/articleDetailsCommentsSlice.test.ts
@@ -3,6 +3,47 @@ import { ArticleDetailsCommentsSchema } from '../types/ArticleDetailsCommentsSch
 
 import { articleDetailsCommentsReducer } from './articleDetailsCommentsSlice';
 
+const data = [
+    {
+        id: 1,
+        text: 'some comment',
+        user: {
+            id: 1,
+            username: 'admin',
+        },
+    },
+    {
+        id: 2,
+        text: 'some comment1',
+        user: {
+            id: 1,
+            username: 'admin',
+        },
+    },
+];
+
+const normalizedData = {
+    entities: {
+        1: {
+            id: 1,
+            text: 'some comment',
+            user: {
+                id: 1,
+                username: 'admin',
+            },
+        },
+        2: {
+            id: 2,
+            text: 'some comment1',
+            user: {
+                id: 1,
+                username: 'admin',
+            },
+        },
+    },
+    ids: [1, 2],
+};
+
 describe('articleDetailsCommentsSlice', () => {
     test('test fetchCommentsByArticleId service pending', () => {
         const state: DeepPartial<ArticleDetailsCommentsSchema> = {
@@ -19,21 +60,22 @@ describe('articleDetailsCommentsSlice', () => {
             error: undefined,
         });
     });
-    // test('test fetchCommentsByArticleId service fulfilled', () => {
-    //     const state: DeepPartial<ArticleDetailsSchema> = {
-    //         isLoading: false,
-    //     };
+    test('test fetchCommentsByArticleId service fulfilled', () => {
+        const state: DeepPartial<ArticleDetailsCommentsSchema> = {
+            isLoading: false,
+        };
 
-    //     expect(
-    //         articleDetailsReducer(
-    //             state as ArticleDetailsSchema,
-    //             fetchArticleById.fulfilled(data, '1', ''),
-    //         ),
-    //     ).toEqual({
-    //         isLoading: false,
-    //         data,
-    //     });
-    // });
+        expect(
+            articleDetailsCommentsReducer(
+                state as ArticleDetailsCommentsSchema,
+                fetchCommentsByArticleId.fulfilled(data, '1', ''),
+            ),
+        ).toEqual({
+            isLoading: false,
+            entities: normalizedData.entities,
+            ids: normalizedData.ids,
+        });
+    });
 
     // test('test fetchCommentsByArticleId service rejected', () => {
     //     const fn = jest.fn();
