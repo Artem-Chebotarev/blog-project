de0f045158a27754521468d074a4dd3708bd6759
Artem Chebotarev <artemchebotarev@Artems-MacBook-Pro.local>
2023-02-14 10:40:48 +0300

    tests for addCommentForArticle are ready

---
+++ src/pages/ArticleDetailsPage/model/services/addCommentForArticle/addCommentForArticle.test.ts
@@ -0,0 +1,122 @@
+import { Article, ArticleBlockType, ArticleType } from '@/entities/Article';
+import { ThunkErrors } from '@/shared/const/common';
+import { TestAsyncThunk } from '@/shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
+
+import { addCommentForArticle } from './addCommentForArticle';
+
+const article: Article = {
+    id: 1,
+    title: 'Javascript news',
+    subtitle: 'Что нового в JS за 2022 год?',
+    img: 'https://teknotower.com/wp-content/uploads/2020/11/js.png',
+    views: 1022,
+    user: {
+        id: 2,
+        username: 'user',
+    },
+    createdAt: '26.02.2022',
+    type: [ArticleType.IT],
+    blocks: [
+        {
+            id: 1,
+            type: ArticleBlockType.TEXT,
+            title: 'Заголовок этого блока',
+            paragraphs: [
+                'Программа, которую по традиции называют «Hello, world!», очень проста. Она выводит куда-либо фразу «Hello, world!», или другую подобную, средствами некоего языка.',
+                'JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.',
+                'Существуют и другие способы запуска JS-кода в браузере. Так, если говорить об обычном использовании программ на JavaScript, они загружаются в браузер для обеспечения работы веб-страниц. Как правило, код оформляют в виде отдельных файлов с расширением .js, которые подключают к веб-страницам, но программный код можно включать и непосредственно в код страницы. Всё это делается с помощью тега <script>. Когда браузер обнаруживает такой код, он выполняет его. Подробности о теге script можно посмотреть на сайте w3school.com. В частности, рассмотрим пример, демонстрирующий работу с веб-страницей средствами JavaScript, приведённый на этом ресурсе. Этот пример можно запустить и средствами данного ресурса (ищите кнопку Try it Yourself), но мы поступим немного иначе. А именно, создадим в каком-нибудь текстовом редакторе (например — в VS Code или в Notepad++) новый файл, который назовём hello.html, и добавим в него следующий код:',
+            ],
+        },
+        {
+            id: 4,
+            type: ArticleBlockType.CODE,
+            code: '<!DOCTYPE html>\n<html>\n  <body>\n    <p id="hello"></p>\n\n    <script>\n      document.getElementById("hello").innerHTML = "Hello, world!";\n    </script>\n  </body>\n</html>;',
+        },
+        {
+            id: 2,
+            type: ArticleBlockType.IMAGE,
+            src: 'https://hsto.org/r/w1560/getpro/habr/post_images/d56/a02/ffc/d56a02ffc62949b42904ca00c63d8cc1.png',
+            title: 'Рисунок 1 - скриншот сайта',
+        },
+    ],
+};
+
+const data = [
+    {
+        id: 1,
+        text: 'some comment',
+        articleId: 1,
+        userId: 1,
+    },
+    {
+        id: 2,
+        text: 'some comment 2',
+        articleId: 1,
+        userId: 1,
+    },
+];
+
+describe('addCommentForArticle', () => {
+    test('success', async () => {
+        const thunk = new TestAsyncThunk(addCommentForArticle, {
+            user: {
+                authData: {
+                    id: 1,
+                    username: 'admin',
+                },
+            },
+            articleDetails: {
+                data: article,
+            },
+        });
+
+        thunk.api.post.mockReturnValue(Promise.resolve({ data }));
+
+        const result = await thunk.callThunk('some comment 2');
+
+        expect(thunk.api.post).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe('fulfilled');
+        expect(result.payload).toEqual(data);
+    });
+
+    test('error due to the absence if text', async () => {
+        const thunk = new TestAsyncThunk(addCommentForArticle, {
+            user: {
+                authData: {
+                    id: 1,
+                    username: 'admin',
+                },
+            },
+            articleDetails: {
+                data: article,
+            },
+        });
+
+        thunk.api.post.mockReturnValue(Promise.resolve({ status: 403 }));
+
+        const result = await thunk.callThunk('');
+
+        expect(result.meta.requestStatus).toBe('rejected');
+        expect(result.payload).toEqual(ThunkErrors.NO_DATA);
+    });
+
+    test('error', async () => {
+        const thunk = new TestAsyncThunk(addCommentForArticle, {
+            user: {
+                authData: {
+                    id: 1,
+                    username: 'admin',
+                },
+            },
+            articleDetails: {
+                data: article,
+            },
+        });
+
+        thunk.api.post.mockReturnValue(Promise.resolve({ status: 403 }));
+
+        const result = await thunk.callThunk('some comment 2');
+
+        expect(result.meta.requestStatus).toBe('rejected');
+    });
+});
